85e4377bb2b5ab60c7b8b5ed0bd3b960d630d796
awesomeusername69420 <24661212+awesomeusername69420@users.noreply.github.com>
2022-01-19 17:59:40 -0500

    Create cl_awesomebot.lua

---
+++ AwesomeBot/cl_awesomebot.lua
@@ -0,0 +1,19 @@
+--[[
+	This file only exists because SendLua is gay
+]]
+
+net.Receive("awesomebot_printcommands", function() -- Help command
+	local a = string.Split(net.ReadString(), "\n")
+
+	for _, v in ipairs(a) do
+		MsgC(Color(222, 222, 222), v .. "\n")
+	end
+end)
+
+net.Receive("awesomebot_prettycolors", function() -- Used to make pretty colors in chat
+	local trollage = net.ReadString()
+
+	if trollage then
+		RunString(trollage)
+	end
+end)
