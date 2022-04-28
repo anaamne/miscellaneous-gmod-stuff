e863992884ca20456c8465c50754fffb795fe9f1
awesomeusername69420 <24661212+awesomeusername69420@users.noreply.github.com>
2022-04-28 17:46:09 -0400

    Update BulletTracers.lua
    accounts for bullet hull and ignore entity

--- BulletTracers.lua
+++ BulletTracers.lua
@@ -7,11 +7,15 @@ local color_red = Color(255, 0, 0, 255)
 hook.Add("EntityFireBullets", "", function(entity, data)
 	if not entity or entity ~= LocalPlayer() or not data then return end
 
-	local tr = util.TraceLine({
+	local hull = Vector(data.HullSize, data.HullSize, data.HullSize)
+
+	local tr = util.TraceHull({
 		start = data.Src,
 		endpos = data.Src + (data.Dir * data.Distance),
+		mins = hull * -1,
+		maxs = hull,
 		mask = MASK_SHOT,
-		filter = entity
+		filter = {entity, data.IgnoreEntity}
 	})
 
 	debugoverlay.Line(data.Src, tr.HitPos, 5, color_red, false)
