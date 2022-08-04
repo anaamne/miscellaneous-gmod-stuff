581ad66e66fa827c1caee400074bedb8da867182
awesomeusername69420 <24661212+awesomeusername69420@users.noreply.github.com>
2022-08-04 19:25:40 -0400

    Update AutoStrafe.lua
    strafe table
    some localization
    enums

--- Cheaterino/AutoStrafe.lua
+++ Cheaterino/AutoStrafe.lua
@@ -5,36 +5,48 @@
 		as_mode - Controls the autostrafe mode. 0 = Disabled; 1 = Legit; 2 = Rage.
 ]]
 
-local Mode = CreateClientConVar("as_mode", 1, false, false, "0 = Disabled; 1 = Legit; 2 = Rage.", 0, 2)
+local Mode = CreateClientConVar("as_mode", 1, false, false, "0 = Disabled; 1 = Legit; 2 = Rage", 0, 2)
 
 local SideMove = GetConVar("cl_sidespeed")
 local ForwardMove = GetConVar("cl_forwardspeed")
 
+local AS_MODE_DISABLED = 0
+local AS_MODE_LEGIT = 1
+local AS_MODE_RAGE = 2
+
+local IN_JUMP = IN_JUMP
+local MOVETYPE_WALK = MOVETYPE_WALK
+
+local Strafers = {}
+
+Strafers[AS_MODE_LEGIT] = function(cmd, Grounded, MaxSideMove)
+	if Grounded then return end
+
+	if cmd:GetMouseX() > 0 then
+		cmd:SetSideMove(MaxSideMove)
+	elseif cmd:GetMouseX() < 0 then
+		cmd:SetSideMove(MaxSideMove * -1)
+	end
+end
+
+Strafers[AS_MODE_RAGE] = function(cmd, Grounded, MaxSideMove, MaxForwardMove, Velocity)
+	Strafers[AS_MODE_LEGIT](cmd, Grounded, MaxSideMove)
+
+	if not cmd:KeyDown(IN_JUMP) then return end
+
+	if Grounded then
+		cmd:SetForwardMove(MaxForwardMove)
+	else
+		cmd:SetForwardMove((MaxForwardMove * 0.5) / Velocity:Length2D())
+		cmd:SetSideMove(cmd:CommandNumber() % 2 == 0 and (MaxSideMove * -1) or MaxSideMove)
+	end
+end
+
 hook.Add("CreateMove", "as", function(cmd)
 	local SetMode = Mode:GetInt()
 
-	if SetMode == 0 then return end
+	if SetMode == AS_MODE_DISABLED then return end
 	if LocalPlayer():GetMoveType() ~= MOVETYPE_WALK or IsValid(LocalPlayer():GetVehicle()) or LocalPlayer():WaterLevel() > 1 then return end
 
-	local Grounded = LocalPlayer():IsOnGround()
-
-	local MaxSideMove = SideMove:GetFloat()
-	local MaxForwardMove = ForwardMove:GetFloat()
-
-	if not Grounded then
-		if cmd:GetMouseX() > 0 then
-			cmd:SetSideMove(MaxSideMove)
-		elseif cmd:GetMouseX() < 0 then
-			cmd:SetSideMove(MaxSideMove * -1)
-		end
-	end
-
-	if SetMode == 2 and cmd:KeyDown(IN_JUMP) then
-		if Grounded then
-			cmd:SetForwardMove(MaxForwardMove)
-		else
-			cmd:SetForwardMove((MaxForwardMove * 0.5) / LocalPlayer():GetVelocity():Length2D())
-			cmd:SetSideMove(cmd:CommandNumber() % 2 == 0 and (MaxSideMove * -1) or MaxSideMove)
-		end
-	end
+	Strafers[SetMode](cmd, LocalPlayer():IsOnGround(), SideMove:GetFloat(), ForwardMove:GetFloat(), LocalPlayer():GetVelocity())
 end)
