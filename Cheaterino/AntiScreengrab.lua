018afe4d6ceda0120c6bbdfe8c89fb2ca645289e
awesomeusername69420 <24661212+awesomeusername69420@users.noreply.github.com>
2022-08-09 18:53:37 -0400

    Update AntiScreengrab.lua

--- Cheaterino/AntiScreengrab.lua
+++ Cheaterino/AntiScreengrab.lua
@@ -1,7 +1,9 @@
 --[[
 	https://github.com/awesomeusername69420/miscellaneous-gmod-stuff
 
-	Works for: https://steamcommunity.com/sharedfiles/filedetails/?id=1342030824
+	Works for:
+		https://steamcommunity.com/sharedfiles/filedetails/?id=1342030824
+		https://steamcommunity.com/sharedfiles/filedetails/?id=2114254167
 ]]
 
 local Len = 4294967295 -- UINT32_MAX; Length of data to send for each part
@@ -14,7 +16,9 @@ local color_white = Color(255, 255, 255, 255)
 local color_yellow = Color(255, 255, 0, 255)
 local color_green = Color(0, 255, 0, 255)
 
-local function cl_rtxappend2(color, text, ply) -- Taken directly out of the addon
+-- Basic screengrab addon
+
+local function cl_rtxappend2(color, text, ply)
 	net.Start("rtxappend2")
 		net.WriteColor(color)
 		net.WriteString(text)
@@ -62,4 +66,10 @@ end)
 
 net.Receive("ScreengrabInterrupted", function()
 	timer.Remove("ScreengrabSendPart")
+end)
+
+-- Gimme that screen
+
+net.Receive("GimmeThatScreen_Request", function()
+	-- Just don't do anything and it'll load infinitely
 end)
